0b79a8161eff900687b78a094c68d943d313d3d5
Mirdad1224 <ali.mirdad@yahoo.com>
2023-03-28 18:32:55 +0330

    create app slice and also define some utility function for modify it

---
+++ src/redux/slices/app.ts
@@ -0,0 +1,182 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { IAppState } from "./../state.type";
+import { AppDispatch, RootState } from "./../store";
+import axios from "../../utils/axios";
+
+const initialState: IAppState = {
+  sideBar: {
+    open: false,
+    type: "CONTACT", // can be CONTACT, STARRED, SHARED
+  },
+  isLoggedIn: true,
+  tab: 0, // [0, 1, 2, 3]
+  snackbar: {
+    open: null,
+    severity: null,
+    message: null,
+  },
+  users: [], // all users of app who are not friends and not requested yet
+  friends: [], // all friends
+  friendRequests: [], // all friend requests
+  chat_type: null,
+  room_id: null,
+};
+
+const slice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    // Toggle Sidebar
+    toggleSideBar(state) {
+      state.sideBar.open = !state.sideBar.open;
+    },
+    updateSideBarType(state, action) {
+      state.sideBar.type = action.payload.type;
+    },
+    updateTab(state, action) {
+      state.tab = action.payload.tab;
+    },
+
+    openSnackBar(state, action) {
+      console.log(action.payload);
+      state.snackbar.open = true;
+      state.snackbar.severity = action.payload.severity;
+      state.snackbar.message = action.payload.message;
+    },
+    closeSnackBar(state) {
+      console.log("This is getting executed");
+      state.snackbar.open = false;
+      state.snackbar.message = null;
+    },
+    updateUsers(state, action) {
+      state.users = action.payload.users;
+    },
+    updateFriends(state, action) {
+      state.friends = action.payload.friends;
+    },
+    updateFriendRequests(state, action) {
+      state.friendRequests = action.payload.requests;
+    },
+    selectConversation(state, action) {
+      state.chat_type = "individual";
+      state.room_id = action.payload.room_id;
+    },
+  },
+});
+
+// Reducer
+export default slice.reducer;
+
+// ----------------------------------------------------------------------
+
+export const closeSnackBar =
+  () => async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(slice.actions.closeSnackBar());
+  };
+
+export const showSnackbar =
+  ({ severity, message }: { severity: any; message: string }) =>
+  async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(
+      slice.actions.openSnackBar({
+        message,
+        severity,
+      })
+    );
+
+    setTimeout(() => {
+      dispatch(slice.actions.closeSnackBar());
+    }, 4000);
+  };
+
+export function ToggleSidebar() {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(slice.actions.toggleSideBar());
+  };
+}
+export function UpdateSidebarType(type: string) {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(slice.actions.updateSideBarType({ type }));
+  };
+}
+export function UpdateTab(tab: number) {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(slice.actions.updateTab({ tab }));
+  };
+}
+
+export function FetchUsers() {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    await axios
+      .get(
+        "/user/get-users",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateUsers({ users: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+export function FetchFriends() {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    await axios
+      .get(
+        "/user/get-friends",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateFriends({ friends: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+export function FetchFriendRequests() {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    await axios
+      .get(
+        "/user/get-requests",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(
+          slice.actions.updateFriendRequests({ requests: response.data.data })
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export const SelectConversation = ({ room_id }: { room_id: string }) => {
+  return async (dispatch: AppDispatch, getState: RootState) => {
+    dispatch(slice.actions.selectConversation({ room_id }));
+  };
+};
