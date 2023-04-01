79f4f47c773dc76f1b366ddbdc9ae629b0caf53b
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-01 10:31:52 +0330

    create redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  useDispatch as useAppDispatch,
+  useSelector as useAppSelector,
+} from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { rootPersistConfig, rootReducer } from "./rootReducer";
+
+const store = configureStore({
+  reducer: persistReducer(rootPersistConfig, rootReducer),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+});
+
+const persistor = persistStore(store);
+
+const { dispatch } = store;
+
+const useSelector = useAppSelector;
+
+const useDispatch = () => useAppDispatch();
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+
+export { store, persistor, dispatch, useSelector, useDispatch };
