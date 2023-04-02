b2e31961f35b3c9a385aa9fac6ec6942ebe8799d
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-02 10:37:21 +0330

    add providers to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import { Provider as ReduxProvider } from "react-redux";
+import SettingsProvider from "./context/SettingsContext";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <ReduxProvider store={store}>
+        <SettingsProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SettingsProvider>
+      </ReduxProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
