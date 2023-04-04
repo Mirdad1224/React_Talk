975d0fa917a7c8660edff31db3d15676be5617fa
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-04 11:10:21 +0330

    refine mui override function

--- src/theme/overrides/index.ts
+++ src/theme/overrides/index.ts
@@ -1,7 +1,6 @@
-import Accordion from './Accordion';
+import { ThemeOptions } from "@mui/material/styles/createTheme";
+import Accordion from "./Accordion";
 
-export default function ComponentsOverrides(theme: any) {
-  return Object.assign(
-    Accordion(theme),
-  );
+export default function ComponentsOverrides(theme: ThemeOptions) {
+  return Object.assign(Accordion(theme));
 }
