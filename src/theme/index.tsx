f3c09a1239ec5ee0e78a2bda517c55f776944182
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-07 09:37:37 +0330

    provide theme to the project

---
+++ src/theme/index.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useMemo } from "react";
+import { CssBaseline } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+  StyledEngineProvider,
+} from "@mui/material/styles";
+import useSettings from "../hooks/useSettings.js";
+import palette from "./palette";
+import typography from "./typography";
+import breakpoints from "./breakpoints";
+import componentsOverride from "./overrides";
+import shadows, { customShadows } from "./shadows";
+
+export default function ThemeProvider({ children }: { children: ReactNode }) {
+  const { themeMode, themeDirection } = useSettings();
+
+  const isLight = themeMode === "light";
+
+  const themeOptions = useMemo(
+    () => ({
+      palette: isLight ? palette.light : palette.dark,
+      typography,
+      breakpoints,
+      shape: { borderRadius: 8 },
+      direction: themeDirection,
+      shadows: isLight ? shadows.light : shadows.dark,
+      customShadows: isLight ? customShadows.light : customShadows.dark,
+    }),
+    [isLight, themeDirection]
+  );
+
+  const theme = createTheme(themeOptions);
+
+  theme.components = componentsOverride(theme);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MUIThemeProvider>
+    </StyledEngineProvider>
+  );
+}
