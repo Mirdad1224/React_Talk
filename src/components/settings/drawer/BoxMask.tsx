6a7742266418919b81bfc41b02a9376e6b6c1dc6
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-12 12:50:11 +0330

    create BoxMask component

---
+++ src/components/settings/drawer/BoxMask.tsx
@@ -0,0 +1,19 @@
+import { Radio, FormControlLabel } from "@mui/material";
+
+export default function BoxMask({ value }: { value: string }) {
+  return (
+    <FormControlLabel
+      label=""
+      value={value}
+      control={<Radio sx={{ display: "none" }} />}
+      sx={{
+        m: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        position: "absolute",
+      }}
+    />
+  );
+}
