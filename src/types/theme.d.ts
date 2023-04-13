e90986a397265d8aff35342d1575c58b7afd6568
Mirdad1224 <ali.mirdad@yahoo.com>
2023-04-13 11:28:26 +0330

    refine mui` color interface

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -4,4 +4,30 @@ declare module "@mui/material/styles" {
   interface ThemeOptions extends ThemeOptions {
     customShadows?: customShadows;
   }
+  interface Theme {
+    customShadows?: customShadows;
+  }
+}
+
+declare module "@mui/material" {
+  interface Color {
+    0: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+    500_8: string;
+    500_12: string;
+    500_16: string;
+    500_24: string;
+    500_32: string;
+    500_48: string;
+    500_56: string;
+    500_80: string;
+  }
 }
