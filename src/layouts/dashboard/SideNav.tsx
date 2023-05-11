2bfed2fd281d0926b6835efe1f92617c1e22d096
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-11 19:30:57 +0330

    create sidenav component

---
+++ src/layouts/dashboard/SideNav.tsx
@@ -0,0 +1,167 @@
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import { Box, Divider, IconButton, Stack } from "@mui/material";
+import AntSwitch from "../../components/AntSwitch";
+import Logo from "../../assets/Images/logo.ico";
+import useSettings from "../../hooks/useSettings";
+import { Nav_Buttons, Nav_Setting } from "../../data";
+import ProfileMenu from "./ProfileMenu";
+import { useNavigate } from "react-router-dom";
+
+const getPath = (index: number) => {
+  switch (index) {
+    case 0:
+      return "/app";
+
+    case 1:
+      return "/group";
+
+    case 2:
+      return "/call";
+
+    case 3:
+      return "/settings";
+
+    default:
+      return "/app";
+  }
+};
+
+const SideBar = () => {
+  const theme = useTheme();
+
+  const navigate = useNavigate();
+
+  const { onToggleMode } = useSettings();
+
+  const [selectedTab, setSelectedTab] = React.useState(0);
+
+  const handleChangeTab = (index: number) => {
+    setSelectedTab(index);
+    navigate(getPath(index));
+  };
+
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: 100,
+
+        backgroundColor:
+          theme.palette.mode === "light"
+            ? "#F0F4FA"
+            : theme.palette.background.paper,
+        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      <Stack
+        py={3}
+        alignItems={"center"}
+        justifyContent="space-between"
+        sx={{ height: "100%" }}
+      >
+        <Stack alignItems={"center"} spacing={4}>
+          <Box
+            sx={{
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+              backgroundColor: theme.palette.primary.main,
+            }}
+            p={1}
+          >
+            <img src={Logo} alt="Tawk" />
+          </Box>
+          <Stack
+            sx={{ width: "max-content" }}
+            direction="column"
+            alignItems={"center"}
+            spacing={3}
+          >
+            {Nav_Buttons.map((el) => {
+              return el.index === selectedTab ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                  p={1}
+                >
+                  <IconButton
+                    onClick={() => {}}
+                    sx={{ width: "max-content", color: "#ffffff" }}
+                  >
+                    {el.icon}
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    handleChangeTab(el.index);
+                  }}
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#080707"
+                        : theme.palette.text.primary,
+                  }}
+                >
+                  {el.icon}
+                </IconButton>
+              );
+            })}
+            <Divider sx={{ width: 48 }} />
+            {Nav_Setting.map((el) => {
+              return el.index === selectedTab ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                  p={1}
+                >
+                  <IconButton
+                    onClick={() => {
+                      // dispatch(UpdateTab(el.index));
+                    }}
+                    sx={{ width: "max-content", color: "#ffffff" }}
+                  >
+                    {el.icon}
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    handleChangeTab(el.index);
+
+                    // dispatch(UpdateTab(el.index));
+                  }}
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#080707"
+                        : theme.palette.text.primary,
+                  }}
+                >
+                  {el.icon}
+                </IconButton>
+              );
+            })}
+          </Stack>
+        </Stack>
+        <Stack spacing={4}>
+          <AntSwitch
+            defaultChecked={theme.palette.mode === "dark"}
+            onChange={onToggleMode}
+          />
+          {/* Profile Menu */}
+          <ProfileMenu />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default SideBar;
