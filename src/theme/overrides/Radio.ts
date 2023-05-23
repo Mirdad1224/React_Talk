481370c6352e3c0869e6f23d72e5c2cad00019f9
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-23 11:44:57 +0330

    override mui's Radio component

---
+++ src/theme/overrides/Radio.ts
@@ -0,0 +1,19 @@
+import { CustomTheme } from "@mui/material";
+
+export default function Radio(theme: CustomTheme) {
+  return {
+    MuiRadio: {
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(1),
+          svg: {
+            fontSize: 24,
+            "&[font-size=small]": {
+              fontSize: 20,
+            },
+          },
+        },
+      },
+    },
+  };
+}
