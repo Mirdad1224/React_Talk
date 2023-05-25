3221da754eb91cf2f30db8ac8ad432929b55d4de
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-25 11:16:32 +0330

    override mui's Slider component

---
+++ src/theme/overrides/Slider.ts
@@ -0,0 +1,29 @@
+import { CustomTheme } from "@mui/material";
+
+export default function Slider(theme: CustomTheme) {
+  const isLight = theme.palette.mode === "light";
+
+  return {
+    MuiSlider: {
+      defaultProps: {
+        size: "small",
+      },
+
+      styleOverrides: {
+        root: {
+          "&.Mui-disabled": {
+            color: theme.palette.action.disabled,
+          },
+        },
+        markLabel: {
+          fontSize: 13,
+          color: theme.palette.text.disabled,
+        },
+        valueLabel: {
+          borderRadius: 8,
+          backgroundColor: theme.palette.grey[isLight ? 800 : 700],
+        },
+      },
+    },
+  };
+}
