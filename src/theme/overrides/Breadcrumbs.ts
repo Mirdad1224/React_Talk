e8e96b4a9f64351aa2c303d88f435081a0faebce
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-27 14:02:13 +0330

    override mui's Breadcrumbs component

---
+++ src/theme/overrides/Breadcrumbs.ts
@@ -0,0 +1,14 @@
+import { CustomTheme } from "@mui/material";
+
+export default function Breadcrumbs(theme: CustomTheme) {
+  return {
+    MuiBreadcrumbs: {
+      styleOverrides: {
+        separator: {
+          marginLeft: theme.spacing(2),
+          marginRight: theme.spacing(2),
+        },
+      },
+    },
+  };
+}
