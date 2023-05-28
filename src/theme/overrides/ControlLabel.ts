94e58b6d7f16bbece7aed864292035347b10ff07
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-28 12:57:19 +0330

    override mui's ControlLabel component

---
+++ src/theme/overrides/ControlLabel.ts
@@ -0,0 +1,27 @@
+import { CustomTheme } from "@mui/material";
+
+export default function ControlLabel(theme: CustomTheme) {
+  return {
+    MuiFormControlLabel: {
+      styleOverrides: {
+        label: {
+          ...theme.typography.body2,
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          marginTop: theme.spacing(1),
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: theme.palette.text.disabled,
+        },
+      },
+    },
+  };
+}
