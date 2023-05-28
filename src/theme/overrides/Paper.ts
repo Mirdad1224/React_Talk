100fe25c6ba59d2328f754603969bd687920a1ad
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-28 12:58:23 +0330

    override mui's Paper component

---
+++ src/theme/overrides/Paper.ts
@@ -0,0 +1,24 @@
+import { CustomTheme } from "@mui/material";
+
+export default function Paper(theme: CustomTheme) {
+  return {
+    MuiPaper: {
+      defaultProps: {
+        elevation: 0,
+      },
+
+      variants: [
+        {
+          props: { variant: "outlined" },
+          style: { borderColor: theme.palette.grey[500_12] },
+        },
+      ],
+
+      styleOverrides: {
+        root: {
+          backgroundImage: "none",
+        },
+      },
+    },
+  };
+}
