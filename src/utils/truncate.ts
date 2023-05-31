fa384e14aa670d15267ca956ca791086125ba234
Mirdad1224 <ali.mirdad@yahoo.com>
2023-05-31 07:52:03 +0330

    create a utility function fot truncating texts

---
+++ src/utils/truncate.ts
@@ -0,0 +1,9 @@
+function truncateString(str: string, num: number) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
+export default truncateString;
