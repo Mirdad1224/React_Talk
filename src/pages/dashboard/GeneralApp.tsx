a0618af03fda7de04bf3636dc1a8d25a893a1a6e
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-02 09:52:03 +0330

    create the main page

---
+++ src/pages/dashboard/GeneralApp.tsx
@@ -0,0 +1,89 @@
+import { useTheme } from "@mui/material/styles";
+import { Box, Stack, Typography } from "@mui/material";
+import { Link, useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ChatComponent from "./Conversation";
+import Chats from "./Chats";
+import Contact from "../../sections/dashboard/Contact";
+import NoChat from "../../assets/Illustration/NoChat";
+import StarredMessages from "../../sections/dashboard/StarredMessages";
+import Media from "../../sections/dashboard/SharedMessages";
+import { RootState } from "../../redux/store";
+
+const GeneralApp = () => {
+  const [searchParams] = useSearchParams();
+
+  const theme = useTheme();
+
+  const { sideBar, room_id, chat_type } = useSelector(
+    (state: RootState) => state.app
+  );
+
+  return (
+    <>
+      <Stack direction="row" sx={{ width: "100%" }}>
+        <Chats />
+        <Box
+          sx={{
+            height: "100%",
+            width: sideBar.open
+              ? `calc(100vw - 740px )`
+              : "calc(100vw - 420px )",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#FFF"
+                : theme.palette.background.paper,
+            borderBottom:
+              searchParams.get("type") === "individual-chat" &&
+              searchParams.get("id")
+                ? "0px"
+                : "6px solid #0162C4",
+          }}
+        >
+          {chat_type === "individual" && room_id !== null ? (
+            <ChatComponent />
+          ) : (
+            <Stack
+              spacing={2}
+              sx={{ height: "100%", width: "100%" }}
+              alignItems="center"
+              justifyContent={"center"}
+            >
+              <NoChat />
+              <Typography variant="subtitle2">
+                Select a conversation or start a{" "}
+                <Link
+                  style={{
+                    color: theme.palette.primary.main,
+                    textDecoration: "none",
+                  }}
+                  to="/"
+                >
+                  new one
+                </Link>
+              </Typography>
+            </Stack>
+          )}
+        </Box>
+        {sideBar.open &&
+          (() => {
+            switch (sideBar.type) {
+              case "CONTACT":
+                return <Contact />;
+
+              case "STARRED":
+                return <StarredMessages />;
+
+              case "SHARED":
+                return <Media />;
+
+              default:
+                break;
+            }
+          })()}
+      </Stack>
+    </>
+  );
+};
+
+export default GeneralApp;
