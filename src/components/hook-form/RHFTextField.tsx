8d934d5d0b9cb55269326b87147175fcaf92cbec
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-03 16:29:12 +0330

    create RHFTextField component

---
+++ src/components/hook-form/RHFTextField.tsx
@@ -0,0 +1,27 @@
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField } from "@mui/material";
+
+export default function RHFTextField({ name, helperText, ...other }: any) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          value={
+            typeof field.value === "number" && field.value === 0
+              ? ""
+              : field.value
+          }
+          error={!!error}
+          helperText={error ? error?.message : helperText}
+          {...other}
+        />
+      )}
+    />
+  );
+}
