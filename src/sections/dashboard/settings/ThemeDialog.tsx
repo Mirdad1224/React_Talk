9b25659932a64b3f5bb771e83ad7a7dbb3eb9495
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-04 10:48:17 +0330

    create ThemeDialog section

---
+++ src/sections/dashboard/settings/ThemeDialog.tsx
@@ -0,0 +1,77 @@
+import { JSXElementConstructor, ReactElement, forwardRef } from "react";
+import {
+  Dialog,
+  Slide,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+} from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
+
+interface IThemeDialogProps {
+  open: boolean;
+  handleClose: () => void;
+}
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return (
+    <Slide direction="up" ref={ref} {...props}>
+      <></>
+    </Slide>
+  );
+});
+
+const ThemeDialog = ({ open, handleClose }: IThemeDialogProps) => {
+  return (
+    <>
+      <Dialog
+        fullWidth
+        open={open}
+        TransitionComponent={
+          Transition as JSXElementConstructor<
+            TransitionProps & { children: ReactElement<any, any> }
+          >
+        }
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle sx={{ mb: 2 }}>{"Choose Theme"}</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="light"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="light"
+                control={<Radio />}
+                label="Light"
+              />
+              <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+              <FormControlLabel
+                value="system"
+                control={<Radio />}
+                label="System Default"
+              />
+            </RadioGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button variant="contained" onClick={handleClose}>
+            Apply
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default ThemeDialog;
