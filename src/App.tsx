1859805f11b5bebc87fbe62cfb65d7e9d755d6e7
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-09 14:07:31 +0330

    refine App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import ThemeSettings from "./components/settings";
+import ThemeProvider from "./theme";
+import Router from "./routes";
+import { closeSnackBar } from "./redux/slices/app";
+import { RootState } from "./redux/store";
+import { AnyAction } from "@reduxjs/toolkit";
+
+const vertical = "bottom";
+const horizontal = "center";
+
+const Alert = React.forwardRef((props: any, ref) => (
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { severity, message, open } = useSelector(
+    (state: RootState) => state.app.snackbar
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <ThemeProvider>
+        <ThemeSettings>
+          <Router />
+        </ThemeSettings>
+      </ThemeProvider>
+
+      {message && open ? (
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          autoHideDuration={4000}
+          key={vertical + horizontal}
+          onClose={() => {
+            console.log("This is clicked");
+            dispatch(closeSnackBar() as unknown as AnyAction);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Alert
+            onClose={() => {
+              console.log("This is clicked");
+              dispatch(closeSnackBar() as unknown as AnyAction);
+            }}
+            severity={severity}
+            sx={{ width: "100%" }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
 export default App;
+
