acc35acc7ae1ee3bb604face4b7473827afc482e
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-27 22:09:32 +0330

    refine root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -4,6 +4,8 @@ import storage from "redux-persist/lib/storage";
 import appReducer from "./slices/app";
 import authReducer from "./slices/auth";
 import conversationReducer from "./slices/conversation";
+import audioCallReducer from "./slices/audioCall";
+import videoCallReducer from "./slices/videoCall";
 
 const rootPersistConfig = {
   key: "root",
@@ -17,6 +19,8 @@ const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
   conversation: conversationReducer,
+  audioCall: audioCallReducer,
+  videoCall: videoCallReducer,
 });
 
 export { rootPersistConfig, rootReducer };
